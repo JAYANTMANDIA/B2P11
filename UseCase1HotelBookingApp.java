c9d456b8a2b3d67f6b423ce26dc272c7374b08ac
JAYANTMANDIA <mandiajayant@gmail.com>
2026-03-12 14:45:54 +0530

    Add UseCase1HotelBookingApp.java for UC1

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,12 @@
+
+public class UseCase1HotelBookingApp {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Book My Stay Hotel Booking System!");
+        System.out.println("Application Name: Book My Stay");
+        System.out.println("Version: 1.0");
+        System.out.println("Thank you for using our system. Enjoy your stay!");
+    }
+}
